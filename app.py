5fc130dbdf621a7134befe7d76ca63e772b0f38f
ChrisEng0212 <cjx02121981@gmail.com>
2022-12-14 12:08:01 +0800

    local set up

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, flash, render_template, redirect, request, jsonify
 import os, json
 import redis
-#from tasks import runStream
 LOCAL = False
 
 try:
     import config
-
     LOCAL = True
     REDIS_URL = config.REDIS_URL
     r = redis.from_url(REDIS_URL, ssl_cert_reqs=None, decode_responses=True)
 except:
     REDIS_URL = os.getenv('CELERY_BROKER_URL')
+    START_CODE = int(os.getenv('START_CODE'))
     r = redis.from_url(REDIS_URL, decode_responses=True)
+    from tasks import runStream
 
 
 print('URL', REDIS_URL)
@@ -68,18 +68,15 @@ def start():
 def add_inputs():
     x = int(request.form['x'] or 0)
 
-    if x == 825:
-        if LOCAL:
-            task = runStream.delay()
-            r.set('task_id', str(task))
-            print('task_id', str(task))
+    if x == START_CODE:
+        task = runStream.delay()
+        r.set('task_id', str(task))
+        print('task_id', str(task))
         flash("Your command has been submitted: " + str(task))
     else:
         flash("Your command has failed: " + str(x))
 
     return redirect('/')
-
-
 
 if __name__ == '__main__':
     app.run()
