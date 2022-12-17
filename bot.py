a61908ab58525fb303d0c29f129d03ffced1e5d9
ChrisEng0212 <cjx02121981@gmail.com>
2022-12-18 01:28:34 +0800

    no response

--- bot.py
+++ bot.py
@@ -17,13 +17,13 @@ except:
     DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
     r = redis.from_url(REDIS_URL, decode_responses=True)
 
-async def send_message(message, user_message, is_private):
-    try:
-        response = responses.get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
+# async def send_message(message, user_message, is_private):
+#     try:
+#         response = responses.get_response(user_message)
+#         await message.author.send(response) if is_private else await message.channel.send(response)
 
-    except Exception as e:
-        print(e)
+#     except Exception as e:
+#         print(e)
 
 
 def run_discord_bot():
@@ -47,27 +47,27 @@ def run_discord_bot():
                 r.set('discord', 'blank')
 
 
-    @client.event
-    async def on_message(message):
+    # @client.event
+    # async def on_message(message):
 
-        ## this stops the infinite loop where bot replies to it's own message
-        if message.author == client.user:
-            return
+    #     ## this stops the infinite loop where bot replies to it's own message
+    #     if message.author == client.user:
+    #         return
 
-        print(message)
+    #     print(message)
 
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
+    #     username = str(message.author)
+    #     user_message = str(message.content)
+    #     channel = str(message.channel)
 
-        print(f'{username} said: "{user_message}" ({channel})')
+    #     print(f'{username} said: "{user_message}" ({channel})')
 
 
-        if user_message[0] == '?':
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
-        else:
-            await send_message(message, user_message, is_private=False)
+    #     if user_message[0] == '?':
+    #         user_message = user_message[1:]
+    #         await send_message(message, user_message, is_private=True)
+    #     else:
+    #         await send_message(message, user_message, is_private=False)
 
     client.run(DISCORD_TOKEN)
 
