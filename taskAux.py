00b543dbaf03717435ff31036192f9dae51def04
ChrisEng0212 <cjx02121981@gmail.com>
2023-04-17 23:40:59 +0800

    order message

--- taskAux.py
+++ taskAux.py
@@ -257,9 +257,9 @@ def marketOrder(side, fraction, stop, profit, mode):
     )
 
     message = order['ret_msg']
-    # data = json.dumps(order['result'])
+    data = json.dumps(order['result'])
 
-    print('ORDER MESSAGE ', + json.dumps(message))
+    print('ORDER MESSAGE ', + data)
 
 
     return True
