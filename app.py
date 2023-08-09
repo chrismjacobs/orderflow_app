e88f50dedb276e0f007428ad8b3719c045505453
chrismjacobs <jacobsen_christian@yahoo.co.uk>
2023-08-09 12:21:50 +0800

    minimal version

--- app.py
+++ app.py
@@ -1,62 +1,20 @@
-from flask import Flask, flash, render_template, redirect, request, jsonify, url_for, make_response
-from time import sleep
+from flask import Flask, render_template,request, jsonify
 import json
-import redis
-import logging
-import os
-import time
-import requests
 from analysis import getBlocks, getVWAP, getImbalances
-from meta import SECRET_KEY, DEBUG, r, LOCAL, START_CODE, RENDER_API,  RENDER_WORKER, auth_required
+from meta import SECRET_KEY, DEBUG, r
 import datetime as dt
-# from tasks import runStream
-
 
 
 app = Flask(__name__)
 app.config['DEBUG'] = DEBUG
 app.config['SECRET_KEY'] = SECRET_KEY
 
-# logging.debug("A debug message")
-# logging.info("An info message")
-# logging.warning("A warning message")
-# logging.error("An error message")
-# logging.critical("A critical message")
-## logging level set to warning and above logging.basicConfig(level=logging.INFO)
-
 @app.route('/')
-@auth_required
 def home():
-
-    if START_CODE == 'block':
-        return redirect('/login')
 
     return render_template('orderflow.html')
 
 @app.route('/setPrices', methods=['POST'])
-def setPrices():
-
-    coinDict = json.loads(request.form['coinOBJ'])
-    reset = request.form['reset']
-
-
-    print(reset, coinDict, type(coinDict))
-
-    if reset == 'true':
-        coinDict['BTC']['deltaswitch']['Sell']['active'] = False
-        coinDict['BTC']['deltaswitch']['Sell']['swing'] = False
-        coinDict['BTC']['deltaswitch']['Sell']['price'] = 0
-
-        coinDict['BTC']['deltaswitch']['Buy']['active'] = False
-        coinDict['BTC']['deltaswitch']['Buy']['swing'] = False
-        coinDict['BTC']['deltaswitch']['Buy']['price'] = 0
-
-        r.set('coinDict', json.dumps(coinDict))
-        r.set('discord_' + 'BTC', 'coinDict Reset')
-    else:
-        r.set('coinDict', json.dumps(coinDict))
-
-    return jsonify({'coinDict' : coinDict})
 
 
 @app.route('/getOF', methods=['POST'])
@@ -64,21 +22,16 @@ def getOF():
     timeBlockSize = int(request.form ['timeBlockSize'])
     coin = request.form ['coin']
 
-    # print('BLOCK SIZES', coin, volumeBlockSize, timeBlockSize)
-
     coinDict = r.get('coinDict')
     coinInfo = json.loads(coinDict)[coin]
     size = coinInfo['volume'][1]
 
+    print(size)
+
     stream = r.get('stream_' + coin)
 
     timeBlocks = json.loads(r.get('timeblocks_' + coin))
-    #timeFlow = r.get('timeflow_' + coin)
-
     timeBlocks = getVWAP(timeBlocks, coin)
-
-    # deltaBlocks = r.get('deltablocks_' + coin)
-    # deltaFlow = r.get('deltaflow_' + coin)
 
     lastHistory = {}
 
@@ -86,7 +39,7 @@ def getOF():
         historyBlocks = json.loads(r.get('history_' + coin))
         if len(historyBlocks) > 0:
             lastHistory = historyBlocks[-1]
-            # print(lastHistory.keys())
+            print(lastHistory.keys())
     except:
         print('NO HISTORY')
 
@@ -104,18 +57,9 @@ def getOF():
     if checkDelta:
         deltaBlocks = json.loads(checkDelta)
 
-    # print('DELTA', deltaBlocks)
-    # print('TIME', timeBlocks)
-
-    # if 'deltablocks_' + coin in lastHistory:
-    #     ## combine History and current
-    #     deltaBlocks = lastHistory['deltablocks_' + coin] + deltaBlocks
-
     volumeBlocks = {}
-    # volumeFlow = {}
 
     volumeCheck = r.get('volumeblocks_' + coin + str(size))
-        # volumeFlow[size] = json.loads(r.get('volumeflow_' + coin + str(size)))
 
     if volumeCheck:
         volumeBlocks = json.loads(volumeCheck)
@@ -125,13 +69,10 @@ def getOF():
         ## combine History and current
         volumeBlocks = lastHistory['volumeblocks_' + coin + str(size)] + volumeBlocks
 
-
-
     for tb in timeBlocks:
         tb['tickList'] = getImbalances(tb['tickList'])
     for vb in volumeBlocks:
         vb['tickList'] = getImbalances(vb['tickList'])
-
 
 
     jDict = {
@@ -151,144 +92,6 @@ def getOF():
 
 
 
-
-@app.route('/serviceAction', methods=['POST'])
-def serviceAction():
-
-    action = request.form ['action']
-
-    ## action = suspend or resume
-
-    url = "https://api.render.com/v1/services/" +  RENDER_WORKER
-
-    headers = {"accept": "application/json", "authorization": 'Bearer ' +  RENDER_API}
-    # payload = {"clearCache": "do_not_clear"}
-
-    if action != 'check':
-        surl = url + "/" + action
-
-        response = requests.post(surl, headers=headers)
-
-        # if action == 'deploy':
-        #     response = requests.post(url, json=payload, headers=headers)
-
-
-        print(response.text)
-
-
-    sResponse = requests.get(url, headers=headers)
-    suspended = json.loads(sResponse.text)['suspended']
-
-    durl = url + "/deploys?limit=20"
-    dResponse = requests.get(durl, headers=headers)
-    print(json.loads(dResponse.text)[0]['deploy'])
-    status = json.loads(dResponse.text)[0]['deploy']['status']
-
-    sDict = {
-        'suspended' : suspended,
-        'status' : status
-    }
-
-    jx = jsonify(sDict)
-
-    # print('JSONIFY X', jDict)
-
-    return jx
-
-@app.route('/start')
-@auth_required
-def start():
-
-    return render_template('start.html')
-
-@app.route('/workerAction', methods=['POST'])
-@auth_required
-def worker():
-    x = int(request.form['passcode'])
-    print('workerAction', x, START_CODE)
-
-    # if x == int(START_CODE):
-    #     block = True
-
-    #     while block:
-    #         t = dt.datetime.today()
-    #         print(t.minute, t.minute%5)
-    #         if t.minute%5 == 0:
-    #             ## multiple of 5 mins
-    #             block = False
-    #         else:
-    #             time.sleep(5)
-
-
-    #     task = runStream.delay()
-    #     r.set('task_id', str(task))
-    #     print('task_id', str(task))
-    #     flash("Your command has been submitted: " + str(task))
-    # else:
-    #     flash("Your command has failed: " + str(x))
-
-    return render_template('start.html')
-
-@app.route('/workerStop', methods=['POST'])
-@auth_required
-def taskend():
-    x = int(request.form['passcode'])
-    print('workerStop', x, START_CODE)
-
-    if x == int(START_CODE):
-        task_id = r.get('task_id')
-        task = runStream.AsyncResult(task_id)
-        task.abort()
-        print('task aborted', str(task))
-        flash("Your command has been submitted: " + str(task))
-    else:
-        flash("Your command has failed: " + str(x))
-
-    return render_template('start.html')
-
-@app.route("/tradingview", methods=['POST'])
-def tradingview_webhook():
-    print('TRADING VIEW ACTION: ')
-    data = json.loads(request.data)
-
-    if data['code'] != SECRET_KEY:
-        return False
-
-    url = "https://api.render.com/v1/services/" +  RENDER_WORKER
-
-    headers = {"accept": "application/json", "authorization": 'Bearer ' +  RENDER_API}
-    # payload = {"clearCache": "do_not_clear"}
-
-    durl = url + "/deploys?limit=20"
-    dResponse = requests.get(durl, headers=headers)
-    print(json.loads(dResponse.text)[0]['deploy'])
-    status = json.loads(dResponse.text)[0]['deploy']['status']
-
-    sResponse = requests.get(url, headers=headers)
-    suspended = json.loads(sResponse.text)['suspended']
-
-    if suspended == 'suspended':
-        surl = url + "/" + 'resume'
-        response = requests.post(surl, headers=headers)
-        print('TV RESUME ' + response.text)
-
-    elif status == 'live':
-        task = runStream.delay()
-        print('TV STREAM STARTED')
-
-
-
-
-
-
-
-
-    return redirect('/')
-
-
-from routesJournal import *
-from routesTrade import *
-
-
 if __name__ == '__main__':
     app.run()
+
--- meta.py
+++ meta.py
@@ -1,44 +1,21 @@
-import boto3
 import os
 import redis
 import json
-from pybit import inverse_perpetual, usdt_perpetual
-from functools import wraps
-from flask import make_response, request
+
 
 
 try:
     import config
-    API_KEY = config.API_KEY
-    API_SECRET = config.API_SECRET
-    AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID
-    AWS_SECRET_ACCESS_KEY = config.AWS_SECRET_ACCESS_KEY
     SECRET_KEY = config.SECRET_KEY
-    REDIS_URL = config.REDIS_URL
-    REDIS_EXTERNAL = config.REDIS_EXTERNAL
-    START_CODE = config.START_CODE
-    LOCAL = True
-    DEBUG = True
-    # r = redis.from_url(REDIS_URL, ssl_cert_reqs=None, decode_responses=True)
-    RENDER_API = config.RENDER_API
-    RENDER_WORKER = config.RENDER_WORKER
-    LOGIN = config.LOGIN
     REDIS_IP = config.REDIS_IP
     REDIS_PASS = config.REDIS_PASS
+    LOCAL = True
+    DEBUG = True
     print('SUCCESS')
 except:
-    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     SECRET_KEY = os.getenv('SECRET_KEY')
-    API_KEY = os.getenv('API_KEY')
-    API_SECRET = os.getenv('API_SECRET')
-    START_CODE = os.getenv('START_CODE')
-    REDIS_URL = os.getenv('CELERY_BROKER_URL')
-    RENDER_API = os.getenv('RENDER_API')
-    RENDER_WORKER = os.getenv('RENDER_WORKER')
     LOCAL = False
     DEBUG = False
-    LOGIN = os.getenv('LOGIN')
     REDIS_IP = os.getenv('REDIS_IP')
     REDIS_PASS = os.getenv('REDIS_PASS')
 
@@ -52,43 +29,3 @@ r = redis.Redis(
     decode_responses=True
     )
 
-try:
-    print('LOGIN', LOGIN, type(LOGIN))
-    LOGIN_DEETS = json.loads(LOGIN)
-    print('LOGIN_DEETS', LOGIN_DEETS, type(LOGIN_DEETS))
-except Exception as e:
-    print('DEETS EXCEPTION', e)
-    LOGIN_DEETS = '{"user": "Fail", "code": "0"}'
-
-def auth_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        auth = request.authorization
-        username = LOGIN_DEETS['user']
-        passcode = LOGIN_DEETS['code']
-
-        if auth and auth.username == username and auth.password == passcode:
-            return f(*args, **kwargs)
-        return make_response("<h1>Access denied!</h1>", 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
-
-    return decorated
-
-
-s3_resource = boto3.resource('s3',
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key= AWS_SECRET_ACCESS_KEY)
-
-session = inverse_perpetual.HTTP(
-    endpoint='https://api.bybit.com',
-    api_key= API_KEY,
-    api_secret=API_SECRET
-)
-
-print('bybit session', session)
-
-session_unauth_USD = usdt_perpetual.HTTP(
-    endpoint="https://api.bybit.com"
-)
-session_unauth_USDT = inverse_perpetual.HTTP(
-    endpoint="https://api.bybit.com"
-)
